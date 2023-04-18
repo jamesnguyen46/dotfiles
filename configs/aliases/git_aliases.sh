5f3bfe51b6fb7a1e137b91a7018c75b99b98cad5
jamesnguyen46 <thachnguyen1989@gmail.com>
2023-04-18 18:13:41 +0700

    Update git aliases

--- configs/aliases/git_aliases.sh
+++ configs/aliases/git_aliases.sh
@@ -5,103 +5,87 @@ if ! command_exists git; then
     return 0
 fi
 
-# Staged Changes
-alias gia='git_add_pn_file_or_directory'
+# ------- Aliases -------
 
-# Show the local branches.
-alias gib='git branch --list'
+# !!! NOT USE ALIAS WITH PREFIX GC OF GCLOUD COMMANDS !!!
 
-# Show/search the remote branches.
-# Input : the name of branch that you want to find. If no, show all remote branches.
-alias gibr='show_remote_branches'
+alias gia="git_stage_changes_n_file_or_directory"
+alias gia~="git_unstage_changes_n_file_or_directory"
 
-# Git commit
-alias gic='git commit --message'
+alias gib="git branch"
+alias gibc="git_branch_checkout_r1_branchname"
+alias gibd="git_branch_delete_o1_branchname"
+alias gibd!="git_branch_remote_delete_o1_branchname"
+alias gibl="git_branch_list_local_o1_branchname"
+alias giblr="git_branch_list_remote_o1_branchname"
+alias gibr="git_branch_rename_r1_old_r2_new"
 
-# Push a empty commit to retry CI pipeline
-# https://dev.to/devsimc/how-to-push-an-empty-commit-28dd
-alias gice='git commit --allow-empty --message "Empty commit to trigger pipeline"'
+alias gic="git_commit_o1_msg"
+alias gicl="git clone"
 
-# Switch branch and pull lastest source code from remote.
-# Input : the branch name.
-alias gicb='checkout_branch'
+alias gidc="git_discard_changes"
+alias gid="git diff"
+alias gids="git diff --staged"
 
-alias gicl='git clone'
+alias gif="git fetch --prune"
+alias gifl="git config --list"
+alias gifu="git_config_user_info_r1_name_r2_email"
 
-# Remove all local branches, except "develop", "dev", "main", "master" and the current branch.
-alias giclr='clean_all_local_branches'
+alias gig="git_logs_o1_number"
 
-# Config git repository
-alias gicf='config_git_repo_r1_name_r2_email'
+alias giha="git stash apply"
+alias gihb="git stash branch"
+alias gihc="git stash clear"
+alias gihd="git stash drop"
+alias gihl="git stash list"
+alias gihp="git stash pop"
+alias gihs="git stash save --include-untracked"
+alias gihw="git stash show"
 
-alias gid='git diff'
+alias gii="git init"
 
-# Fetch the remote branches.
-alias gif='git fetch --prune'
+alias gil="git pull"
 
-# Pull the lastest source code.
-alias gil='git pull'
+alias gim="git merge"
+alias gima="git merge --abort"
+alias gimc="git merge --continue"
+alias gimq="git merge --quit"
 
-# Show the log of current branch.
-# Input : the log number. Default is 10.
-alias gig='show_log'
+alias gip="git push"
+alias gip!="git push --force"
 
-# Git push
-alias gip='git push'
-alias gipf='git push -f'
+alias girb="git rebase"
+alias girba="git rebase --abort"
+alias girbc="git rebase --continue"
+alias girbq="git rebase --quit"
 
-# Reset the HEAD commit.
-# Input : the number of HEAD commit
-alias gir='reset_commit'
+alias gire="git revert"
+alias girs="git_reset_o1_commit_no_from_head"
 
-alias gis='git status'
+alias gis="git status"
+
+alias gist="git log --stat"
+
+alias giw="git show"
+
+alias giy="git cherry-pick"
+alias giya="git cherry-pick --abort"
+alias giyc="git cherry-pick --continue"
+alias giyq="git cherry-pick --quit"
 
 # ------- FUNCTIONS -------
 
-git_add_pn_file_or_directory() {
-    if [[ $# -eq 0 ]]; then
-        git add .
-    else
-        git add "$@"
-    fi
+git_stage_changes_n_file_or_directory() {
+    [[ $# -eq 0 ]] && git add --verbose --all || git add --verbose "$@"
 }
 
-reset_commit() {
-    if [[ -n "$1" ]]; then
-        git reset --soft HEAD~"$1"
-    else
-        git reset --soft HEAD^
-    fi
+git_unstage_changes_n_file_or_directory() {
+    [[ $# -eq 0 ]] && git reset || git reset -- "$@"
 }
 
-config_git_repo_r1_name_r2_email() {
-    git config user.name "$1"
-    git config user.email "$2"
-    git config core.pager "less -FX"
-}
-
-clean_all_local_branches() {
-    keep_branch="$(git branch --show-current)\|develop\|dev\|main\|master"
-    git branch | grep -v "$keep_branch" | xargs git branch -D
-}
-
-show_log() {
-    [[ -n "$1" ]] && log_number=$1 || log_number=10
-    git log -"$log_number" --pretty='%C(yellow)%h %C(cyan)%cd%C(auto)%d %C(reset)%s %C(blue)<%ae>' --graph --branches --date=format:'%Y-%m-%d %H:%M:%S' --date-order --abbrev-commit
-}
-
-show_remote_branches() {
-    if [[ $# -eq 0 ]]; then
-        git branch --remotes
-    else
-        git branch --remotes | grep "$1"
-    fi
-}
-
-checkout_branch() {
+git_branch_checkout_r1_branchname() {
     # Check if input parameter exists
     if [[ $# -eq 0 ]]; then
-        echo "Usage : $0 <branch_name>"
         return
     fi
 
@@ -111,7 +95,6 @@ checkout_branch() {
     # Check if branch is local branch.
     # Then pull the lastest changes from remote repository
     if [[ "$branch_name" == "${current_branch:2}" ]]; then
-        # Check if branch is current branch
         if [ "${current_branch:0:1}" != "*" ]; then
             git checkout "$branch_name"
         else
@@ -135,4 +118,74 @@ checkout_branch() {
             git checkout -b "$branch_name"
         fi
     fi
+}
+
+git_branch_list_local_o1_branchname() {
+    [[ $# -eq 0 ]] && git branch --list || git branch --list | grep "$1"
+}
+
+git_branch_list_remote_o1_branchname() {
+    [[ $# -eq 0 ]] && git branch --remotes || git branch --remotes | grep "$1"
+}
+
+git_branch_rename_r1_old_r2_new() {
+    git branch -m "$1" "$2"
+}
+
+git_branch_delete_o1_branchname() {
+    [[ $# -eq 0 ]] && {
+        keep_branch="$(git branch --show-current)\|develop\|dev\|main\|master"
+        git branch | grep -v "$keep_branch" | xargs git branch -D
+    } || {
+        git branch -D "$1"
+    }
+}
+
+git_branch_remote_delete_o1_branchname() {
+    [[ $(git_branch_list_remote_o1_branchname "$1") ]] && {
+        git push origin -d "$1" &&
+            git_branch_delete_o1_branchname "$1"
+    } || {
+        echo "Branch name \"$1\" not found."
+    }
+}
+
+git_commit_o1_msg() {
+    if [[ -n "$1" ]]; then
+        git commit --message "$1"
+        return
+    fi
+
+    # Commit an empty commit to retry CI pipeline
+    echo "Do you want to commit an empty commit (Y/N)?"
+    read -r response
+    if [[ $response == "y" || $response == "Y" ]]; then
+        git commit --allow-empty --message "Empty commit to trigger pipeline"
+    fi
+}
+
+git_discard_changes() {
+    git clean -df
+    if [[ $(git status -suno) ]]; then
+        git checkout -- .
+    fi
+}
+
+git_config_user_info_r1_name_r2_email() {
+    git config user.name "$1"
+    git config user.email "$2"
+}
+
+git_logs_o1_number() {
+    [[ -n "$1" ]] && log_number=$1 || log_number=10
+    git log -"$log_number" \
+        --pretty='%C(yellow)%h %C(cyan)%cd%C(auto)%d %C(reset)%s %C(blue)<%ae>' \
+        --graph --branches \
+        --date=format:'%Y-%m-%d %H:%M:%S' \
+        --date-order \
+        --abbrev-commit
+}
+
+git_reset_o1_commit_no_from_head() {
+    [[ -n "$1" ]] && git reset --soft HEAD~"$1" || git reset --soft HEAD^
 }
